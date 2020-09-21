9befa89f5a26a8eed13efcd17ca4d7d7c4a04fb7
Tom Grigg <tomegrigg@gmail.com>
2020-09-21 14:21:01 -0700

    i9844: workaround test

--- i9844/src/main/scala/Main.scala
+++ i9844/src/main/scala/Main.scala
@@ -1,9 +1,15 @@
-trait Foo[A]
+trait Foo[A] {
+  def test(x: A, y: A): Boolean
+}
 
 trait Baz[A]  {
   type Q = A
+  val baz: A
+
   trait Bar {
     this: Foo[A] =>
-    def bar(a: Q): Unit
+    def bar(a: Q): Unit = {
+      test(a, baz)
+    }
   }
 }
