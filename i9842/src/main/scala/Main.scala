455a4af8ee5145fd98367570d7c83936f78f1409
Tom Grigg <tomegrigg@gmail.com>
2020-09-21 18:12:50 +0000

    i9842: final minimization

--- i9842/src/main/scala/Main.scala
+++ i9842/src/main/scala/Main.scala
@@ -2,13 +2,13 @@ trait Foo[+A] {
   def foo[A1 >: A](a: A1): Foo[A1]
 }
 
-trait Bar[-C]
+trait Bar[-B]
 
 trait X {
   def bla[A](m: Bar[A]): Foo[A]
-  def baz(x: Bar[Int]): Foo[Int] = {
+
+  def baz(x: Bar[Int]): Unit = {
     var t = bla(x)
-    t = t.foo(1)
-    return t
+    t = t.foo(0)
   }
 }
