5c3627cd966e7a1801481c725c632fd0ea3aa7d4
Tom Grigg <tomegrigg@gmail.com>
2020-09-21 10:49:36 -0700

    i9842: iteration 6

--- i9842/build.sbt
+++ i9842/build.sbt
@@ -1,5 +1,5 @@
-val dottyVersion = "0.27.0-RC1"
-// val dottyVersion = "2.13.3"
+// val dottyVersion = "0.27.0-RC1"
+val dottyVersion = "2.13.3"
 
 lazy val root = project
   .in(file("."))
--- i9842/src/main/scala/Main.scala
+++ i9842/src/main/scala/Main.scala
@@ -1,10 +1,14 @@
 trait Foo[+A] {
-  def foo[A1 >: A](a: A1): A
+  def foo[A1 >: A](a: A1): Foo[A1]
 }
 
 trait Bar[-C]
 
 trait X {
   def bla[A](m: Bar[A]): Foo[A]
-  def baz(x: Bar[Int]): Int = bla(x).foo(2) + 1
+  def baz(x: Bar[Int]): Foo[Int] = {
+    var t = bla(x)
+    t = t.foo(1)
+    return t
+  }
 }
