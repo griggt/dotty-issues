087b9ad6f928935e0f1efb80c3500bb204c3dfd9
Tom Grigg <tomegrigg@gmail.com>
2020-09-29 13:47:13 -0700

    i7128 standalone: third pass on RecorderMacro

--- i7128/standalone/src/main/scala/Recorder.scala
+++ i7128/standalone/src/main/scala/Recorder.scala
@@ -1,8 +1,5 @@
 import scala.language.experimental.macros
 
 abstract class Recorder[A, R] {
-  def listener: RecorderListener[A, R] = ???
-
-  inline def apply(value: A): R = ${ RecorderMacro.apply('value, 'listener) }
-  //inline def apply(value: A, message: => String): R = ${ RecorderMacro.apply('value, 'message, 'listener) }
+  inline def apply(value: A): R = ${ RecorderMacro.apply('value) }
 }
--- i7128/standalone/src/main/scala/RecorderMacro.scala
+++ i7128/standalone/src/main/scala/RecorderMacro.scala
@@ -6,12 +6,11 @@ class RecorderMacro(using qctx0: QuoteContext) {
 
   private[this] val runtimeSym: Symbol = '[RecorderRuntime[_, _]].unseal.tpe.typeSymbol
 
-  def apply[A: Type, R: Type](recording: Expr[A], message: Expr[String], listener: Expr[RecorderListener[A, R]]): Expr[R] = {
+  def apply[A: Type, R: Type](recording: Expr[A]): Expr[R] = {
     val termArg: Term = recording.unseal.underlyingArgument
 
     '{
-      val recorderRuntime: RecorderRuntime[A, R] = new RecorderRuntime($listener)
-      recorderRuntime.recordMessage($message)
+      val recorderRuntime: RecorderRuntime[A, R] = ???
       ${
         Block(
           recordExpressions('{ recorderRuntime }.unseal, termArg),
@@ -30,15 +29,15 @@ class RecorderMacro(using qctx0: QuoteContext) {
   }
 
   private def recordAllValues(runtime: Term, expr: Term): Term =
-    recordValue(runtime, recordSubValues(runtime, expr), expr)
+    recordValue(runtime, recordSubValues(runtime, expr))
 
   private def recordSubValues(runtime: Term, expr: Term): Term =
     expr match {
-      case Apply(x, ys)     => Apply(recordAllValues(runtime, x), ys.map(recordAllValues(runtime, _)))
-      case _                => expr
+      case Apply(x, ys) => Apply(recordAllValues(runtime, x), ys.map(recordAllValues(runtime, _)))
+      case _            => expr
     }
 
-  private def recordValue(runtime: Term, expr: Term, origExpr: Term): Term = {
+  private def recordValue(runtime: Term, expr: Term): Term = {
     val recordValueSel: Term = {
       val m = runtimeSym.method("recordValue").head
       runtime.select(m)
@@ -48,15 +47,15 @@ class RecorderMacro(using qctx0: QuoteContext) {
       //case Ident(_) if skipIdent(expr.symbol) => expr
       case _ => // TG return just `expr` here prevents the crash
         val tapply = recordValueSel.appliedToType(expr.tpe)
-        Apply.copy(expr)(tapply, List(expr,Literal(Constant(0))))
+        Apply.copy(expr)(tapply, List(expr, Literal(Constant(0))))
     }
   }
 
 }
 
 object RecorderMacro {
-  def apply[A: Type, R: Type](recording: Expr[A], listener: Expr[RecorderListener[A, R]])(using qctx: QuoteContext): Expr[R] =
-    new RecorderMacro().apply(recording, '{""}, listener)
+  def apply[A: Type, R: Type](recording: Expr[A])(using QuoteContext): Expr[R] =
+    new RecorderMacro().apply(recording)
 }
 
 object StringRecorderMacro {}
