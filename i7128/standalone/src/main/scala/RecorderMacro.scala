a98a88d3cae59a7f475a4e074b7fa1ac6ccb9831
Tom Grigg <tomegrigg@gmail.com>
2020-09-29 12:50:45 -0700

    i7128 standalone: first pass on RecorderMacro

--- i7128/standalone/src/main/scala/RecorderMacro.scala
+++ i7128/standalone/src/main/scala/RecorderMacro.scala
@@ -1,16 +1,12 @@
 import scala.quoted._
 
 class RecorderMacro(using qctx0: QuoteContext) {
-  // https://dotty.epfl.ch/docs/reference/metaprogramming/tasty-reflect.html#sealing-and-unsealing
   import qctx0.tasty.{ Type => _, _ }
   import util._
 
   private[this] val runtimeSym: Symbol = '[RecorderRuntime[_, _]].unseal.tpe.typeSymbol
 
-  def apply[A: Type, R: Type](
-      recording: Expr[A],
-      message: Expr[String],
-      listener: Expr[RecorderListener[A, R]]): Expr[R] = {
+  def apply[A: Type, R: Type](recording: Expr[A], message: Expr[String], listener: Expr[RecorderListener[A, R]]): Expr[R] = {
     val termArg: Term = recording.unseal.underlyingArgument
 
     '{
@@ -25,44 +21,18 @@ class RecorderMacro(using qctx0: QuoteContext) {
     }
   }
 
-  def apply2[A: Type, R: Type](
-      expected: Expr[A],
-      found: Expr[A],
-      message: Expr[String],
-      listener: Expr[RecorderListener[A, R]]): Expr[R] = {
-    val expectedArg: Term = expected.unseal.underlyingArgument
-    val foundArg: Term = found.unseal.underlyingArgument
-
-    '{
-      val recorderRuntime: RecorderRuntime[A, R] = new RecorderRuntime($listener)
-      recorderRuntime.recordMessage($message)
-      ${
-        Block(
-          recordExpressions('{ recorderRuntime }.unseal, expectedArg) :::
-          recordExpressions('{ recorderRuntime }.unseal, foundArg),
-          '{ recorderRuntime.completeRecording() }.unseal
-        ).seal.cast[R]
-      }
-    }
-  }
-
   private[this] def recordExpressions(runtime: Term, recording: Term): List[Term] = {
-    val source = getSourceCode(recording)
     val ast = recording.showExtractors
 
     val resetValuesSel: Term = {
       val m = runtimeSym.method("resetValues").head
       runtime.select(m)
     }
-    try {
-      List(
-        Apply(resetValuesSel, List()),
-        recordExpression(runtime, source, ast, recording)
-      )
-    } catch {
-      case e: Throwable => throw new RuntimeException(
-        "Expecty: Error rewriting expression.\nText: " + source + "\nAST : " + ast, e)
-    }
+
+    List(
+      Apply(resetValuesSel, List()),
+      recordExpression(runtime, "", ast, recording)
+    )
   }
 
   // emit recorderRuntime.recordExpression(<source>, <tree>, instrumented)
@@ -80,40 +50,33 @@ class RecorderMacro(using qctx0: QuoteContext) {
       ))
   }
 
-  private[this] def recordAllValues(runtime: Term, expr: Term): Term =
-    expr match {
-      case New(_)     => expr
-      case Literal(_) => expr
-      case Typed(r @ Repeated(xs, y), tpe) => recordSubValues(runtime, r)
-      // don't record value of implicit "this" added by compiler; couldn't find a better way to detect implicit "this" than via point
-      case Select(x@This(_), y) if expr.pos.start == x.pos.start => expr
-      // case x: Select if x.symbol.isModule => expr // don't try to record the value of packages
-      case _ => recordValue(runtime, recordSubValues(runtime, expr), expr)
-    }
+  private def recordAllValues(runtime: Term, expr: Term): Term =
+    recordValue(runtime, recordSubValues(runtime, expr), expr)
+    // expr match {
+      // case New(_)     => ???
+      // case Literal(_) => ???
+      // case Typed(r @ Repeated(xs, y), tpe) => ??? //recordSubValues(runtime, r)
+      //case _ => recordValue(runtime, recordSubValues(runtime, expr), expr)
+    // }
 
-  private[this] def recordSubValues(runtime: Term, expr: Term): Term =
+  private def recordSubValues(runtime: Term, expr: Term): Term =
     expr match {
-      case Apply(x, ys) =>
-        try {
-          Apply(recordAllValues(runtime, x), ys.map(recordAllValues(runtime, _)))
-        } catch {
-          case e: AssertionError => expr
-        }
-      // case TypeApply(x, ys) => recordValue(TypeApply.copy(expr)(recordSubValues(x), ys), expr)
-      case TypeApply(x, ys) => TypeApply.copy(expr)(recordSubValues(runtime, x), ys)
-      case Select(x, y)     => Select.copy(expr)(recordAllValues(runtime, x), y)
-      case Typed(x, tpe)    => Typed.copy(expr)(recordSubValues(runtime, x), tpe)
-      case Repeated(xs, y)  => Repeated.copy(expr)(xs.map(recordAllValues(runtime, _)), y)
+      case Apply(x, ys)     => Apply(recordAllValues(runtime, x), ys.map(recordAllValues(runtime, _)))
+      //case TypeApply(x, ys) => ??? //TypeApply.copy(expr)(recordSubValues(runtime, x), ys)
+      //case Select(x, y)     => ??? //Select.copy(expr)(recordAllValues(runtime, x), y)
+      //case Typed(x, tpe)    => ??? //Typed.copy(expr)(recordSubValues(runtime, x), tpe)
+      //case Repeated(xs, y)  => ??? //Repeated.copy(expr)(xs.map(recordAllValues(runtime, _)), y)
       case _                => expr
     }
 
-  private[this] def recordValue(runtime: Term, expr: Term, origExpr: Term): Term = {
-    // debug
-    // println("recording " + expr.showExtractors + " at " + getAnchor(expr))
+  private def recordValue(runtime: Term, expr: Term, origExpr: Term): Term = {
     val recordValueSel: Term = {
       val m = runtimeSym.method("recordValue").head
       runtime.select(m)
     }
+
+    // TEG overriding skipIdent to `true` prevents the crash
+    /*
     def skipIdent(sym: Symbol): Boolean =
       sym.fullName match {
         case "scala" | "java" => true
@@ -121,75 +84,24 @@ class RecorderMacro(using qctx0: QuoteContext) {
         case fullName if fullName.startsWith("java.")  => true
         case _ => false
       }
+    */
 
-    def skipSelect(sym: Symbol): Boolean = {
-      (sym match {
-        case sym if sym.isDefDef => sym.signature.paramSigs.nonEmpty
-        case sym if sym.isValDef => skipIdent(sym)
-        case _ => true
-      })
-
-    }
     expr match {
-      case Select(_, _) if skipSelect(expr.symbol) => expr
-      case TypeApply(_, _) => expr
-      case Ident(_) if skipIdent(expr.symbol) => expr
-      case _ =>
+      //case Select(_, _) => ??? //expr
+      //case TypeApply(_, _) => ??? //expr
+      //case Ident(_) if skipIdent(expr.symbol) => expr
+      case _ => // TG return just `expr` here prevents the crash
         val tapply = recordValueSel.appliedToType(expr.tpe)
-        Apply.copy(expr)(
-          tapply,
-          List(
-            expr,
-            Literal(Constant(getAnchor(expr)))
-          )
-        )
+        Apply.copy(expr)(tapply, List(expr,Literal(Constant(0))))
     }
   }
 
-  private[this] def getSourceCode(expr: Tree): String = {
-    val pos = expr.pos
-    (" " * pos.startColumn) + pos.sourceCode
-  }
-
-  private[this] def getAnchor(expr: Term): Int =
-    expr match {
-      case Apply(x, ys) if x.symbol.fullName == "verify.asserts.RecorderRuntime.recordValue" && ys.nonEmpty =>
-        getAnchor(ys.head)
-      case Apply(x, ys)     => getAnchor(x) + 0
-      case TypeApply(x, ys) => getAnchor(x) + 0
-      case Select(x, y)     =>
-        expr.pos.startColumn + math.max(0, expr.pos.sourceCode.indexOf(y))
-      case _                => expr.pos.startColumn
-    }
+  //private[this] def getSourceCode(expr: Tree): String = ""
 }
 
 object RecorderMacro {
-  def apply[A: Type, R: Type](
-      recording: Expr[A],
-      listener: Expr[RecorderListener[A, R]])(using qctx: QuoteContext): Expr[R] =
+  def apply[A: Type, R: Type](recording: Expr[A], listener: Expr[RecorderListener[A, R]])(using qctx: QuoteContext): Expr[R] =
     new RecorderMacro().apply(recording, '{""}, listener)
-
-  /** captures a method invocation in the shape of assert(expr, message). */
-  def apply[A: Type, R: Type](
-      recording: Expr[A],
-      message: Expr[String],
-      listener: Expr[RecorderListener[A, R]])(using qctx: QuoteContext): Expr[R] =
-    new RecorderMacro().apply(recording, message, listener)
 }
 
-object StringRecorderMacro {
-  /** captures a method invocation in the shape of assertEquals(expected, found). */
-  def apply[R: Type](
-      expected: Expr[String],
-      found: Expr[String],
-      listener: Expr[RecorderListener[String, R]])(using qctx: QuoteContext): Expr[R] =
-    new RecorderMacro().apply2[String, R](expected, found, '{""}, listener)
-
-  /** captures a method invocation in the shape of assertEquals(expected, found). */
-  def apply[R: Type](
-      expected: Expr[String],
-      found: Expr[String],
-      message: Expr[String],
-      listener: Expr[RecorderListener[String, R]])(using qctx: QuoteContext): Expr[R] =
-    new RecorderMacro().apply2[String, R](expected, found, message, listener)
-}
+object StringRecorderMacro {}
