8a721371c2b238b076a6456e0a787f06163a693d
Tom Grigg <tomegrigg@gmail.com>
2020-10-23 00:44:33 -0700

    i10068a: checkpoint 6f

--- i10068a/build.sbt
+++ i10068a/build.sbt
@@ -1,7 +1,9 @@
 val dottyVersion = "0.27.0-RC1"
 
-val zioversion = "1.0.0-RC1+23-6a9b7831+20201022-1706-SNAPSHOT"   // broken make() impl
-//val zioversion = "1.0.0-RC1+24-ff300925+20201022-1835-SNAPSHOT" // fixed make() impl
+//val zioversion = "1.0.0-RC1+23-6a9b7831+20201022-1706-SNAPSHOT"   // broken make() impl
+//val zioversion = "1.0.0-RC1+24-ff300925+20201022-1835-SNAPSHOT" // fixed make() impl [maybe not any more, it was overwritten]
+//val zioversion = "1.0.0-RC1+41-4b718e25-SNAPSHOT"
+val zioversion = "1.0.0-RC1+45-b98cc083-SNAPSHOT"
 
 lazy val root = project
   .in(file("."))
--- i10068a/src/main/scala/Main.scala
+++ i10068a/src/main/scala/Main.scala
@@ -1,14 +1,42 @@
-import zio.prelude.Ord
+import zio.prelude.{ Ord, PartialOrd }
 
 // Woohoo ! This one blows up!
 object Main {
-  def main(args: Array[String]): Unit = {
+  def viaDummyLaw(): Unit = {
     val law = Ord.dummyLaw2
     val x = Some(1)
     val y = Some(2)
     law.apply(x, x)  // ok
     law.apply(x, y)  // blows up
     //law.apply(Some(1), Some(1))    // blows up
+  }
+
+  ///////////////////////////////////////////////
+
+  def doIt[A: Ord](a: A, b: A): Boolean = {
+    println(a)
+    val o = implicitly[Ord[A]]
+    println(o)
+    val r = o.compare(a, b)
+    println(r)   // This is ok!
+
+    val oo = implicitly[PartialOrd[A]]
+    val rr = oo.compare(a, b)   // But this blows up. It is similar to what lessOrEqual is doing
+    println(s"rr = $rr")
+
+    true
+  }
+
+  def directly(): Unit = {
+    val x = Some(1)
+    val y = Some(2)
+    doIt(x, x)   // ok  -- edit: not anymore. something changed by checkpoint 6f (1.0.0-RC1+45-b98cc083-SNAPSHOT)
+    //doIt(x, y)   // kablammo
+  }
+
+  def main(args: Array[String]): Unit = {
+    //viaDummyLaw()
+    directly()
   }
 }
 
