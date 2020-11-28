c419d79585505aab435bef3e02fbfef38a4179c2
Tom Grigg <tomegrigg@gmail.com>
2020-11-28 14:39:51 -0800

    i10366 standalone: remove Position from AnyShouldWrapper

--- i10366/standalone/Lib.scala
+++ i10366/standalone/Lib.scala
@@ -3,13 +3,11 @@ import scala.compiletime.testing._
 trait Position
 
 object Test:
-  class AnyShouldWrapper[T](val lhs: T, val pos: Position):
+  class AnyShouldWrapper[T](val lhs: T):
     def shouldBe(right: Any): Unit = ???
 
   implicit inline def here: Position = ???
-
-  implicit def convertToAnyShouldWrapper[T](o: T)(implicit pos: Position): AnyShouldWrapper[T] =
-    AnyShouldWrapper(o, pos)
+  implicit def convertToAnyShouldWrapper[T](o: T)(implicit pos: Position): AnyShouldWrapper[T] = ???
 
   def test(): Unit =
     typeChecks("class Foo") shouldBe true
