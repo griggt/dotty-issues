5c6c987c8a82781e2f5a5fd580c69d37622b8396
Tom Grigg <tomegrigg@gmail.com>
2020-11-28 15:03:01 -0800

    i10366 standalone: remove Foo/Position implicit altogether

--- i10366/standalone/Lib.scala
+++ i10366/standalone/Lib.scala
@@ -1,13 +1,7 @@
 import scala.compiletime.testing._
 
-trait Foo
-
 object Test:
-  class ShouldWrapper(lhs: Boolean):
+  implicit class ShouldWrapper(lhs: Boolean):
     def shouldBe(rhs: Boolean): Unit = ???
 
-  implicit val foo: Foo = ???
-  implicit def convertToShouldWrapper(x: Boolean)(implicit f: Foo): ShouldWrapper = ???
-
-  def test(): Unit =
-    typeChecks("class Bar") shouldBe true
+  typeChecks("class Bar") shouldBe true
