8e44e6c9408b957f10d983404932f59bca2604f3
Tom Grigg <tomegrigg@gmail.com>
2020-11-30 19:24:12 -0800

    cp 6

--- i10567/standalone/src/main/scala/Main.scala
+++ i10567/standalone/src/main/scala/Main.scala
@@ -2,6 +2,5 @@ import org.apache.avro.SchemaBuilder
 
 object Test {
   def bldr = SchemaBuilder.builder()
-  //def oops = bldr.fixed("foo")
-  def oops = bldr.record("Hello")
+  def oops = bldr.fixed("foo")
 }
