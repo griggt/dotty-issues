b54438db8b226394bbd69eaec887d349ab8bb37d
Tom Grigg <tomegrigg@gmail.com>
2020-12-17 15:01:05 -0800

    i10827: final

--- i10827/lib/Lib.scala
+++ i10827/lib/Lib.scala
@@ -1,19 +1,12 @@
 package test.http4s
 
-case class Method(name: String)
+class Method(name: String)
 
 object Method {
-  sealed trait Semantics
-  object Semantics {
-    trait Safe extends Semantics
-  }
-
-  sealed trait PermitsBody extends Method
-
-  import Semantics._
+  trait Safe
+  trait PermitsBody extends Method
 
   type SafeMethodWithBody = Method with Safe with PermitsBody
-
   val GET: SafeMethodWithBody = new Method("GET") with Safe with PermitsBody
 }
 
