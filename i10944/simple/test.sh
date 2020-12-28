17deb243902f30782ce97b553945a40313ae4f48
Tom Grigg <tomegrigg@gmail.com>
2020-12-28 12:46:57 -0800

    i10944: updated simple test script

--- i10944/simple/test.sh
+++ i10944/simple/test.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-dotc=/opt/scala3-3.0.0-M3/bin/scalac
+dotc=/src/dotty/bin/scalac
+#dotc=/opt/scala3-3.0.0-M3/bin/scalac
+#dotc=scalac
+
+java -version; javac -version ; ${dotc} -version
 
 rm -f *.class *.tasty
 javac TypeToken.java && ${dotc} Test.scala
