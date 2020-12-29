77bee470099fa630fd8c92322a39556b526b8d8b
Tom Grigg <tomegrigg@gmail.com>
2020-12-28 18:56:46 -0800

    i9566 standalone: make Resource a trait

--- i9566/standalone/src/main/scala/Test.scala
+++ i9566/standalone/src/main/scala/Test.scala
@@ -7,7 +7,7 @@ object IO {
   implicit def applicativeForIO: Applicative[IO] = ???
 }
 
-sealed abstract class Resource[+F[_], +A] {
+trait Resource[+F[_], +A] {
   def map[G[x] >: F[x], B](f: A => B)(implicit F: Applicative[G[*]]): Resource[G[*], B] = ???
 }
 
